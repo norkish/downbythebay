5163da067b1c0ee320a18f1cbe0ccb0bf1314c39
Ben Bay <benbay@Ben-Bays-Laptop.local>
2017-05-27 17:17:03 -0600

    added pos and phoneme enums

--- DownByTheBay/src/linguistic/Pos.java
+++ DownByTheBay/src/linguistic/Pos.java
@@ -1,6 +1,41 @@
 package linguistic;
 
-// TODO: replace with Ben's enumerated type for POS
+//This is the Penn-Treebank part of speech tagset, used by Stanford CoreNLP
 public enum Pos {
-	DETERMINER, NOUN, VERB, ADJECTIVE, ADVERB
+	CC,
+	CD,
+	DT,
+	EX,
+	FW,
+	IN,
+	JJ,
+	JJR,
+	JJS,
+	LS,
+	MD,
+	NN,
+	NNS,
+	NNP,
+	NNPS,
+	PDT,
+	POS,
+	PRP,
+	PRP$,
+	RB,
+	RBR,
+	RBS,
+	RP,
+	SYM,
+	TO,
+	UH,
+	VB,
+	VBD,
+	VBG,
+	VBN,
+	VBP,
+	VBZ,
+	WDT,
+	WP,
+	WP$,
+	WRB
 }
